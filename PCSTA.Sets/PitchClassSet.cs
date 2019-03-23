ab0277c93c2af861a4913f193cf3285d3166ca2e
drjknowles <41568009+drjknowles@users.noreply.github.com>
2019-03-23 23:07:05 +0000

    Initial Add of empty project PCSTA

---
+++ PCSTA.Sets/PitchClassSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCSTA.Sets
+{
+    public class PitchClassSet
+    {
+        public int Cardinality { get; set; }
+
+        public int Position { get; set; }
+
+        public byte[] PrimeForm { get; set; }
+
+    }
+}
