1e636c44d27fdba0612342ba48a832ccd8250f7c
drjknowles <41568009+drjknowles@users.noreply.github.com>
2019-04-02 22:43:41 +0100

    Created Initial Project solution

---
+++ PCSTA.Sets.Tests/Forte/PrimeFormTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PCSTA.Sets.Tests
+{
+    [TestClass]
+    public class PrimeFormTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PrimeForm testPrimeForm = new PrimeForm();
+        }
+    }
+}
---
+++ PCSTA.Sets/Forte/PrimeForm.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PCSTA.Sets
+{
+    public class PrimeForm
+    {
+    }
+}
